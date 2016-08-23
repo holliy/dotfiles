27fb9a53df01bee3497e4a112173a173ce3ecfa6
holliy <20940515+holliy@users.noreply.github.com>
2016-08-24 07:52:09 +0900

    Add a script for Linux

---
+++ mklink.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+IFS_BAK=$IFS
+IFS=$'\n'
+
+WD=`pwd`
+DIR=($(find dotfiles/ -mindepth 1 -type d))
+FILE=($(find dotfiles/ -type f))
+
+IFS=$IFS_BAK
+
+# 途中のディレクトリが存在しないとき作成
+for d in "${DIR[@]}"; do
+  dd=${d/dotfiles/$HOME}
+  if [ ! -e "${dd}" ]; then
+    # ファイル・フォルダが存在しない
+    mkdir "${dd}"
+  elif [ ! -d "${dd}" ]; then
+    # フォルダが存在しない
+    echo "Cannot create ${dd}"
+  fi
+done
+
+for f in "${FILE[@]}"; do
+  df=${f/dotfiles/$HOME}
+  if [ ! -f "${df}" ]; then
+    ln -s ${WD}/${f} ${df}
+  fi
+done
