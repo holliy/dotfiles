5bfbe66e2281e5f0701b69587ac6c768b7949624
holliy <20940515+holliy@users.noreply.github.com>
2016-08-23 20:02:11 +0900

    Create missing folders

--- mklink.bat
+++ mklink.bat
@@ -3,24 +3,44 @@
 REM 遅延環境変数の有効化
 setlocal ENABLEDELAYEDEXPANSION
 
-REM 管理者権限で実行
+REM スクリプトが置かれている場所をカレントディレクトリにする
 cd /d %~dp0
 
-for /f "tokens=3 delims=\ " %%i in ('whoami /groups^|find "Mandatory"') do set LEVEL=%%i
+REM 管理者権限で実行
+for /f "tokens=3 delims=\ " %%i in ('%SYSTEMROOT%\System32\whoami.exe /groups^|%SYSTEMROOT%\System32\find "Mandatory"') do set LEVEL=%%i
 
 if NOT "%LEVEL%"=="High" (
   @powershell -NoProfile -ExecutionPolicy unrestricted -Command "Start-Process %~f0 -Verb runas"
   exit /b 0
 )
 
+
 cd dotfiles
 
 REM カレントディレクトリを取得
 for /f "usebackq tokens=*" %%i in (`cd`) do set WD=%%i
 
+REM 途中のディレクトリが存在しないとき作成
+for /f "usebackq tokens=*" %%i in (`dir /ad /b /s`) do (
+  set LINE=%%i
+  set DIR=!LINE:%WD%=%USERPROFILE%!
+
+  if NOT EXIST "!DIR!" (
+    REM ファイル・フォルダが存在しない
+    mkdir !DIR!
+  ) else if NOT EXIST "!DIR!\" (
+    REM フォルダが存在しない
+    mshta vbscript:execute^("MsgBox(""Cannot create !DIR!""),16:close"^)
+  )
+)
+
+
 for /f "usebackq tokens=*" %%i in (`dir /a-d /b /s`) do (
   set LINE=%%i
+  set FILE=!LINE:%WD%=%USERPROFILE%!
 
-  REM ファイルのパスからカレントディレクトリを置換してリンク作成
-  mklink !LINE:%WD%=%USERPROFILE%! !LINE!
+  if NOT EXIST "!FILE!" (
+    REM ファイルのパスからカレントディレクトリを置換してリンク作成
+    mklink !LINE:%WD%=%USERPROFILE%! !LINE!
+  )
 )
