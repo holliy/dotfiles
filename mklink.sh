ac72829e9908d8c75a5023e3410075e5832f4f56
holliy <20940515+holliy@users.noreply.github.com>
2025-08-11 20:15:52 +0900

    Install dotter using Homebrew

--- mklink.sh
+++ mklink.sh
@@ -6,12 +6,32 @@ cd "$(dirname "$0")"
 # terminfoファイルのコンパイル
 ls -1 terminfo/* | xargs -n1 tic -x
 
+# homebrewをインストール
+if ! { which brew >/dev/null 2>&1; }; then
+  echo "Installing homebrew..."
+  if ! { which curl >/dev/null 2>&1; }; then
+    echo "curl is required to install homebrew"
+    exit 1
+  fi
+
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if [ $? -ne 0 ]; then
+    exit 1
+  fi
+
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
 DOTTER=dotter
 
 if ! { which $DOTTER >/dev/null 2>&1; }; then
-  echo "$DOTTER: not found in \$PATH"
-  echo "Download from https://github.com/SuperCuber/dotter/releases"
-  exit 1
+  echo "Installing dotter..."
+  brew install dotter
+fi
+
+# 既存のファイルを退避
+if [ -f ~/.bashrc ] && [ ! -L ~/.bashrc ]; then
+  mv ~/.bashrc ~/.bashrc.bak
 fi
 
 cd dotfiles/
